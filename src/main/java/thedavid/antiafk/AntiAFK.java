2e032ea079e24a00f3a63578e7f2266e76cb0696
theDAVID543 <theDAVID5432@gmail.com>
2023-07-02 19:50:05 +0800

    added comment

--- src/main/java/thedavid/antiafk/AntiAFK.java
+++ src/main/java/thedavid/antiafk/AntiAFK.java
@@ -17,12 +17,15 @@ import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.jetbrains.annotations.NotNull;
 
+import java.io.IOException;
 import java.util.*;
 
 public final class AntiAFK extends JavaPlugin implements Listener, CommandExecutor {
     Map<Player,Integer> playerTime = new HashMap<>();
     Map<Player,Boolean> playerNeedToClick = new HashMap<>();
     Map<Player,Integer> playerRandomCommand = new HashMap<>();
+    public final Integer detectAFKTime = 300;
+    public final Integer minPlayer = 20;
     List<NamedTextColor> colors = Arrays.asList(
             NamedTextColor.AQUA,
             NamedTextColor.BLACK,
@@ -48,15 +51,20 @@ public final class AntiAFK extends JavaPlugin implements Listener, CommandExecut
             @Override
             public void run() {
                 Bukkit.getServer().getOnlinePlayers().forEach(p -> {
+//                    玩家在線時間 +1
                     playerTime.putIfAbsent(p,0);
                     playerTime.put(p,playerTime.get(p) + 1);
-                    if(playerTime.get(p) >= 3 && Bukkit.getOnlinePlayers().size() > 1 && !Objects.equals(playerNeedToClick.get(p), true)){
-                        int random = getRandom(0,1000);
+//                    玩家人數 >= minPlayer && 玩家在線時間 >= detectAFKTime
+                    if(playerTime.get(p) >= detectAFKTime && Bukkit.getOnlinePlayers().size() >= minPlayer && !Objects.equals(playerNeedToClick.get(p), true)){
+//                        隨機密碼(防止自動打指令)
+                        int random = getRandom(0,9999);
+//                        隨機顏色(防止自動偵測顏色)
                         Random rand = new Random();
                         int randomColor = rand.nextInt(colors.size());
                         playerRandomCommand.put(p, random);
                         p.sendMessage(Component.text("---------------------"));
                         p.sendMessage(Component.text("我們偵測到您正在掛機 請於2分鐘內點擊按鈕來證明您還在線").color(NamedTextColor.RED));
+//                        隨機位置(防止直接自動點擊)
                         TextComponent text = Component.text().build();
                         int randomI = getRandom(0,40);
                         for(int i = 0;i<randomI;i++){
@@ -92,13 +100,27 @@ public final class AntiAFK extends JavaPlugin implements Listener, CommandExecut
     }
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String s, @NotNull String[] args) {
-        if(playerNeedToClick.get((Player) sender).equals(true) && playerRandomCommand.get((Player) sender).equals(Integer.parseInt(args[0]))){
-            playerTime.put((Player) sender,0);
-            playerNeedToClick.put((Player) sender, false);
-            playerRandomCommand.remove((Player) sender);
-            sender.sendMessage(
-                    Component.text("已成功驗證").color(NamedTextColor.GREEN)
-            );
+        if(args.length >= 1){
+            try{
+//                確認玩家需要驗證 && 密碼正確
+                if(playerNeedToClick.get((Player) sender).equals(true) && playerRandomCommand.get((Player) sender).equals(Integer.parseInt(args[0]))){
+                    playerTime.put((Player) sender,0);
+                    playerNeedToClick.put((Player) sender, false);
+                    playerRandomCommand.remove((Player) sender);
+                    sender.sendMessage(
+                            Component.text("已成功驗證").color(NamedTextColor.GREEN)
+                    );
+                }else{
+                    sender.sendMessage(
+                            Component.text("驗證失敗").color(NamedTextColor.RED)
+                    );
+                }
+            } catch (Exception ex){
+                sender.sendMessage(
+                        Component.text("驗證失敗").color(NamedTextColor.RED)
+                );
+            }
+
         }
         return true;
     }
